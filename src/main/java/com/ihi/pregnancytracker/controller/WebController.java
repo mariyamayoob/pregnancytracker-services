c1589b489e04d16234e7715b8506b9f3c25ab98c
Mariyam Mohamed Ayoob <mayoob3@gatech.edu>
2020-11-16 18:00:24 -0500

    remove unused imports

--- src/main/java/com/ihi/pregnancytracker/controller/WebController.java
+++ src/main/java/com/ihi/pregnancytracker/controller/WebController.java
@@ -1,15 +1,7 @@
 package com.ihi.pregnancytracker.controller;
 
-import com.ihi.pregnancytracker.beans.AppUser;
-import com.ihi.pregnancytracker.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 @Controller
 public class WebController {
