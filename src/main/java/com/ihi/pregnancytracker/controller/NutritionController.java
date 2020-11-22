9ecaca5a09448f0bc71834737d74e0c2fedeef4f
mariyamayoob <74500760+mariyamayoob@users.noreply.github.com>
2020-11-22 12:13:59 -0500

    Update NutritionController.java

--- src/main/java/com/ihi/pregnancytracker/controller/NutritionController.java
+++ src/main/java/com/ihi/pregnancytracker/controller/NutritionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", ,allowedHeaders = "*")
 @RestController
 public class NutritionController {
   
