8e9563a12509868aea50290595c2f2ddbe3273b7
mariyamayoob <74500760+mariyamayoob@users.noreply.github.com>
2020-11-22 09:14:33 -0500

    Added Cors

--- src/main/java/com/ihi/pregnancytracker/controller/NutritionController.java
+++ src/main/java/com/ihi/pregnancytracker/controller/NutritionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class NutritionController {
   
