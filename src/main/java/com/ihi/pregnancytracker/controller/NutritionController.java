d0edc87cd33dc8a5bbceae790245e3ce5f084a54
Mariyam Mohamed Ayoob <mayoob3@gatech.edu>
2020-11-22 12:25:07 -0500

    fix cors header

--- src/main/java/com/ihi/pregnancytracker/controller/NutritionController.java
+++ src/main/java/com/ihi/pregnancytracker/controller/NutritionController.java
@@ -5,11 +5,12 @@ import com.ihi.pregnancytracker.service.NutritionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", ,allowedHeaders = "*")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
 @RestController
 public class NutritionController {
   
